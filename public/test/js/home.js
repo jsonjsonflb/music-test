e676214a63f3ed379a06981e4cfa15731649a35c
zhangyan5 <1115625887@qq.com>
2019-05-04 22:32:49 +0800

    DENGLU

---
+++ public/test/js/home.js
@@ -0,0 +1,8 @@
+$(function () {
+    $('.ph_div').on('mousemove',function () {
+        $('.amonts').fadeIn(500);
+    });
+    $('.ph_div').on('mouseleave',function () {
+        $('.amonts').fadeOut(500);
+    });
+});
