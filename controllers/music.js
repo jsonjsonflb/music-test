8c7568ebe45d2fab5abc75da56da6a972927f05c
Json <jsonjsonflb@gmail.com>
2019-05-05 11:38:38 +0800

    调整

--- controllers/music.js
+++ controllers/music.js
@@ -1,4 +1,4 @@
-const musicModel = require('../models/music')
+const musicModel = require('../models/music');
 const path = require('path');
 
 function upload(ctx) {
@@ -6,44 +6,44 @@ function upload(ctx) {
   // console.log(ctx.request.files)
   // console.log(ctx.request.body)
   // 1.获取字符串数据
-  let {title, singer, time} = ctx.request.body;
+  let { title, singer, time } = ctx.request.body;
   // 2.获取文件->保存文件的网络路径（方便/public请求返回）
-  let {file, filelrc} = ctx.request.files;
-
-
+  let { file, filelrc } = ctx.request.files;
 
   let saveSingObj = {
-    title, singer, time
-  }
+    title,
+    singer,
+    time
+  };
   // 2.5 歌词可选
-  saveSingObj.filelrc = '';// 没有歌词
+  saveSingObj.filelrc = ''; // 没有歌词
   if (filelrc) {
     // 文件路径 文件名+后缀
-    saveSingObj.filelrc = '/public/files/' + path.parse(filelrc.path).base
+    saveSingObj.filelrc = '/public/files/' + path.parse(filelrc.path).base;
   }
 
   if (!file) {
-    ctx.throw('歌曲必须上传')
+    ctx.throw('歌曲必须上传');
   }
   // 处理歌曲路径
-  saveSingObj.file = '/public/files/' + path.parse(file.path).base
+  saveSingObj.file = '/public/files/' + path.parse(file.path).base;
 
   // 加入用户ID 未来用session
   saveSingObj.uid = 1;
 
-  return saveSingObj
+  return saveSingObj;
 }
 
 module.exports = {
   async addMusic(ctx, next) {
-    let saveSingObj = upload(ctx)
+    let saveSingObj = upload(ctx);
     // 3.插入数据到数据库
-    let result = await musicModel.addMusicByObj(saveSingObj)
+    let result = await musicModel.addMusicByObj(saveSingObj);
     // 4.响应结果给用户
     ctx.body = {
       code: '001',
       msg: result.message
-    }
+    };
   },
   /**
    * 更新音乐
@@ -53,23 +53,24 @@ module.exports = {
    */
   async updateMusic(ctx, next) {
     let saveSingObj = upload(ctx);
-    let {id} = ctx.request.body;
-    Object.assign(saveSingObj, {id})
+    let { id } = ctx.request.body;
+    Object.assign(saveSingObj, { id });
     // 更新数据
-    let result = await musicModel.updateMusic(saveSingObj)
+    let result = await musicModel.updateMusic(saveSingObj);
     // update music set title=?, singer=?..
     // where id = ?
     if (result.affectedRows !== 1) {
       ctx.body = {
-        code: '002', msg: result.message
-      }
-      return
+        code: '002',
+        msg: result.message
+      };
+      return;
     }
     ctx.body = {
-      code: '001', msg: '更新成功'
-    }
+      code: '001',
+      msg: '更新成功'
+    };
   },
-
 
   async deleteMusic(ctx, next) {
     // 接收id
@@ -77,11 +78,12 @@ module.exports = {
     let result = await musicModel.deleteMusicById(id);
     if (result.affectedRows === 0) {
       ctx.throw('删除失败：' + result.message);
-      return
+      return;
     }
     ctx.body = {
-      code: '001', msg: '删除成功'
-    }
+      code: '001',
+      msg: '删除成功'
+    };
   },
 
   async showEdit(ctx, next) {
@@ -93,13 +95,13 @@ module.exports = {
     if (musics.length === 0) {
       // 异常 错误页面
       ctx.throw('歌曲不存在');
-      return
+      return;
     }
     let music = musics[0];
     ctx.render('edit', {
       music,
       user: ctx.session.user
-    })
+    });
   },
 
   // 首页
@@ -111,42 +113,41 @@ module.exports = {
 
     ctx.render('index', {
       musics
-    })
+    });
   },
 
   // 上传图片页面
   async showPicUpload(ctx, next) {
-    ctx.render('upload')
+    ctx.render('upload');
   },
   // 上传图片地址
   async addPicture(ctx, next) {
     // 1.获取字符串数据
-    let {picType} = ctx.request.body;
+    let { picType } = ctx.request.body;
     let uid = ctx.session.user.id;
     // 2.获取文件->保存文件的网络路径（方便/public请求返回）
     let fileList = ctx.request.files;
     let fileLegth = Object.keys(fileList).length;
     let fileObjList = Object.values(fileList);
 
-
-    if(fileLegth===0) {
-      ctx.throw('必须上传图片')
+    if (fileLegth === 0) {
+      ctx.throw('必须上传图片');
     }
 
     let dataLIst = []; //数据列表
     // 遍历文件列表
-    fileObjList.forEach((item,index)=>{
+    fileObjList.forEach((item, index) => {
       dataLIst.push({
         picLink: '/public/files/' + path.parse(item.path).base,
         picType,
         uid,
         title: '1',
         content: '1',
-        createTime: new Date().getTime(),
-      })
-    })
+        createTime: new Date().getTime()
+      });
+    });
 
     let data = await musicModel.addPicyByObj(dataLIst);
-    console.log(data)
+    console.log(data);
   }
-}
+};
